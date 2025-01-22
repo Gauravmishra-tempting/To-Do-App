23e5e74540dc73828b12009e9e4d1314daa70171
vishwajeettec <vishwajeetsingh@tecraki.io>
2025-01-22 14:51:18 +0530

    add new code

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Loader from './components/loader/Loader';
+
+const Todoapp = lazy(() => import('./components/pages/Todoapp'));
+
+function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Todoapp />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../loader/Loader.css';
+
+const loader = () => {
+  return (
+    // <!-- From Uiverse.io by Nawsome --> 
+    <div className='Loader-wrapper'>
+    <div className="loader"></div>
+    </div>
+  )
+}
+
+export default loader
