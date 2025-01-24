eb58073af5690f647ab5a2baab5ae35f11ac637c
vishwajeettec <vishwajeetsingh@tecraki.io>
2025-01-24 15:08:59 +0530

    add time date and clear button

--- src/components/pages/Todoapp.jsx
+++ src/components/pages/Todoapp.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 const  Todoapp = () => {
   const [inputText, setInputText] = useState("");
   const [task, setTask] = useState([]);
-  
+  const [dateTime, setDateTime] = useState("");
 
 
   const handleChange = (value) => {
@@ -22,13 +23,38 @@ const  Todoapp = () => {
     setInputText("");
 
     }
-  
 
+    // const removeTask = (index) => {
+    //   setTask((prevTask) => prevTask.filter((_, i) => i !== index));
+    // }
+
+    
+// Date and Time 
+
+    setInterval(() => {
+      const now = new Date();
+      const formateDate = now.toLocaleDateString();
+      const formateTime = now.toLocaleTimeString();
+      setDateTime(`${formateDate} - ${formateTime}`);
+    }, 1000);
+
+
+    // Clear all tasks
+    const handleClear = () => {
+      setInputText("");
+      setTask([]);
+    };
 
 
   return (
    <div className="container">
     <div className="row">
+      <div className="col-md-12 text-center mt-5">
+        <h1>To-Do App</h1>
+      </div>
+      <div className="col-md-12 text-center mt-3">
+        <h5>{dateTime}</h5>
+      </div>
       <div className="col-md-6 offset-md-3 mt-5">
         <form onSubmit={handleFormSubmit} className="input-form input-group">
         <input type="text" 
@@ -54,8 +80,21 @@ const  Todoapp = () => {
           {task.map((item, index) => (
             <li key={index} className='list-group-item'>
               {item}
+              <button
+              className='align-items-left'
+              // onClick={() => removeTask(index)}
+              >
+                <RiDeleteBin6Line />
+              </button>
             </li>
           ))}
+          <div className='mx-auto mt-3'>
+            <button onClick={handleClear}
+            type='button' 
+            className="btn btn-danger">
+              Clear
+            </button>
+          </div>
         </ul>
       </div>
     </div>
